ff18bfeb749bf02a562db7ac59cc28acccdc50fd
Antone Evans <antoneev65@gmail.com>
2021-06-02 07:14:36 -0400

    Fix for pdf rendering

--- articleAssistance/app.py
+++ articleAssistance/app.py
@@ -42,7 +42,7 @@ def displayPDF(file):
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
