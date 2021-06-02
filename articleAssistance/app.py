422df719c77401c2e146d1bc7d034e1a218ce986
Antone Evans <antoneev65@gmail.com>
2021-06-02 07:03:16 -0400

    Fix for pdf rendering

--- articleAssistance/app.py
+++ articleAssistance/app.py
@@ -37,16 +37,16 @@ def createFiles():
 
 # Displaying PDF files on UI
 def displayPDF(file):
-    wb.open_new_tab(file)
-    # # Opening file from file path
-    # with open(file, "rb") as f:
-    #     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    #
-    # # Embedding PDF in HTML
-    # pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-    #
-    # # Displaying File
-    # st.markdown(pdf_display, unsafe_allow_html=True)
+    st.write(os.path.abspath(file))
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 def main():
     # On page load or clicking the Home selection
