ab45f9d645a79a4e9f61593951fe69568ed94d06
Antone Evans <antoneev65@gmail.com>
2021-06-03 17:42:53 -0400

    Deleting test file

--- articleAssistance/test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-# Creating DB connection
-connection = sqlite3.connect("data.db")
-cursor = connection.cursor()
-
-cursor.execute("SELECT * FROM keywords WHERE word='"+'american'+"'")
-print(cursor.fetchone())
-
